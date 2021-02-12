7636b7328760d40bfce1f345e3b8e045c77ccba7
Vibhor Singh <vibdaddy12@gmail.com>
2021-02-12 13:52:16 +0100

    Update PrimeService.cs

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -6,9 +6,10 @@ namespace PrimeService
     {
         public bool IsPrime(int candidate)
         {
-            if(candidate == 1){
+            if(candidate == 2){
                 return true;
             }
+
             throw new NotImplementedException("Please create a test first.");
         }
 
